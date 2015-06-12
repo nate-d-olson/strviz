0583b72c221eb113c79e65a3d5f64d522c7e6eb8
Nate Olson <nathandavidolson@gmail.com>
2015-06-12 13:35:46 -0400

    changed df2 to df for simplicity, added libraries, renamed functions to be more descriptive

--- process_straitrazor_txt.R
+++ process_straitrazor_txt.R
@@ -1,7 +1,12 @@
+library(dplyr)
+library(tidyr)
+library(magrittr)
 
-process_txtfile <- function(txt_file, read){
-    df2 <- try(read.table(txt_file,sep =",", stringsAsFactors=F))
-    if(class(df2)=='try-error') {
+process_allelecalls_file <- function(txt_file, read){
+    # no need to name differently from process_seqeunces as they are in 
+    # different environments, they can't see eachother
+    df <- try(read.table(txt_file,sep =",", stringsAsFactors=F))
+    if(class(df)=='try-error') {
         warning(paste0(txt_file, "is empty"))
         return()
     }
@@ -11,14 +16,14 @@ process_txtfile <- function(txt_file, read){
          
 }
 
-
-process_directory2 <- function(root_dir, paired = TRUE){
-    df2 <- data_frame()
+## changed name to be more descriptive
+process_allelecalls_directory <- function(root_dir, paired = TRUE){
+    df <- data_frame()
     if(paired){
         for(read in c("R1", "R2")){
             directory <- paste0(root_dir,"/", read,"/")
             for(txt_file in list.files(directory, "txt",full.names = TRUE)){
-         df2 %<>% bind_rows(process_txtfile(txt_file,read))
+                df %<>% bind_rows(process_allelecalls_file(txt_file,read))
             }
             
         }
@@ -29,7 +34,7 @@ process_directory2 <- function(root_dir, paired = TRUE){
     df
 }
 
-
+process_allelecalls_directory("H12-H-ZT80925_S96_L001_R1_001.fastq.STRaitRazor/PE_MiSeq_ALL/")
 
 
 
