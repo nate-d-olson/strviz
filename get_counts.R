f575f6a9149e5fcab9ef8c33f557d5cca6c8dfa9
Nate Olson <nathandavidolson@gmail.com>
2015-06-24 09:38:13 -0400

    fixed bug in seq_df, converting into functions

--- get_counts.R
+++ get_counts.R
@@ -1,26 +1,34 @@
 ## geting seq counts for R1, R2 and directions 1 and 2
 source("process_straitrazor_output.R")
-seq_df <- process_sequences_directory("H12-H-ZT80925_S96_L001_R1_001.fastq.STRaitRazor/PE_MiSeq_ALL/") %>% 
-            mutate(rev_seq = revcom(seq_df$Sequence)) %>% 
+source("revcom.R")
+seq_dir <- "H12-H-ZT80925_S96_L001_R1_001.fastq.STRaitRazor/PE_MiSeq_ALL/"
+seq_df <- process_sequences_directory(seq_dir) %>%  
+            mutate(rev_seq = revcom(Sequence)) %>% 
             spread(Read, Allele_Calls)
 
-seq_comp2 <- seq_df %>% rename(rev_seq = Sequence, Sequence = rev_seq, D2_R1 = R1, D2_R2 = R2)
-seq_comp<- seq_df %>% rename(D1_R1 = R1, D1_R2 = R2)
+seq_D2 <- seq_df %>% 
+                rename(rev_seq = Sequence, 
+                       Sequence = rev_seq, 
+                       D2_R1 = R1, D2_R2 = R2)
 
-seq_count <- left_join(seq_comp2, seq_comp)
+seq_D1<- seq_df %>% rename(D1_R1 = R1, D1_R2 = R2)
+
+seq_D1D2 <- left_join(seq_D1, seq_D2)
 
 
-
-new_seq <- data_frame()
-for(i in unique(seq_count$Locus)){
-    locus_count <- seq_count %>% filter(Locus == i)
-    locus_seq <- locus_count[1,]
-    for(j in 2:nrow(locus_count)){
-        if(!(locus_count[j,]$rev_seq %in% locus_seq$Sequence)){
-            locus_seq <- bind_rows(locus_seq, locus_count[j,])
-        }
-    }
-    new_seq <- bind_rows(new_seq, locus_seq)
+## Function to remove duplicate allele entries from count data frame
+remove_duplicates <- function (str_count_df) {
+  seq_count <- data_frame()
+  for(i in unique(str_count_df$Locus)){
+      locus_D1D2 <- str_count_df %>% filter(Locus == i)
+      locus_count <- locus_D1D2[1,]
+      for(j in 2:nrow(locus_count)){
+          if(!(locus_D1D2[j,]$rev_seq %in% locus_count$Sequence)){
+              locus_count <- bind_rows(locus_count, locus_D1D2[j,])
+          }
+      }
+      seq_count <- bind_rows(seq_count, locus_count)
+  }
 }
 
 
