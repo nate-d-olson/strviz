ab10848b925d060601648a4124c2ce3d4d6751d5
Winston <sew@PN107082.campus.nist.gov>
2015-06-26 14:27:01 -0400

    added comments to non majority peaks

--- calc_str_metrics.R
+++ calc_str_metrics.R
@@ -15,7 +15,7 @@ new_seq[is.na(new_seq)] <- 0
 new_seq$Coverage_of_Majority_Peaks <- (new_seq$D2_R1 + new_seq$D2_R2 + new_seq$D1_R1 + new_seq$D1_R2)
 new_seq2 <- new_seq  %>% 
     group_by(Locus)  %>%  
-    top_n(2)
+        top_n(2)
 
 #preliminary code for genotype calls (.2 can be changed to any other ratio if needed)
 new_seq2 <- new_seq2 %>% 
@@ -31,8 +31,10 @@ new_seq2 <- new_seq2 %>%
 
 #preliminary code for Read Bias
 ungroup(new_seq2)
-new_seq2 <- new_seq2  %>%  mutate(Sum_R1 = (D2_R1 + D1_R1))
-new_seq2 <- new_seq2  %>%  mutate(Sum_R2 = (D2_R2 + D1_R2))
+new_seq2 <- new_seq2  %>%  
+    mutate(Sum_R1 = (D2_R1 + D1_R1))
+new_seq2 <- new_seq2  %>%  
+    mutate(Sum_R2 = (D2_R2 + D1_R2))
 new_seq2 <- new_seq2 %>%
     rowwise() %>%
     mutate(Read_Bias = min(c(Sum_R1, Sum_R2))/Coverage_of_Majority_Peaks)
@@ -40,13 +42,15 @@ new_seq2 <- new_seq2 %>%
 
 #preliminary code for Strand Bias
 ungroup(new_seq2)
-new_seq2 <- new_seq2  %>%  mutate(Sum_D1 = (D1_R1 + D1_R2))
-new_seq2 <- new_seq2  %>%  mutate(Sum_D2 = (D2_R1 + D2_R2))
+new_seq2 <- new_seq2  %>%
+    mutate(Sum_D1 = (D1_R1 + D1_R2))
+new_seq2 <- new_seq2  %>%  
+    mutate(Sum_D2 = (D2_R1 + D2_R2))
 new_seq2 <- new_seq2 %>%
     rowwise() %>%
     mutate(Strand_Bias = min(c(Sum_D1, Sum_D2))/Coverage_of_Majority_Peaks)
 
-#Prelimiary coverage of non majority peaks (need to figure out how to do this all in one data frame)
+#Prelimiary coverage of non majority peaks (need to figure out how to do this all in one data frame, and not include the stutter alleles)
 new_seq3 <- new_seq  %>% 
 group_by(Locus, Allele)  %>%  
 mutate(Percentage_of_non_Majority_peaks = (sum(Coverage_of_Majority_Peaks)-max(Coverage_of_Majority_Peaks))/(sum(Coverage_of_Majority_Peaks)))
