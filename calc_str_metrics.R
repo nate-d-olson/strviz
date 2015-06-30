67e6b955bce4532f2873d960e27dc554391acbd7
Winston <sew@PN107082.campus.nist.gov>
2015-06-30 11:56:32 -0400

    Added some of the descriptions.

--- calc_str_metrics.R
+++ calc_str_metrics.R
@@ -1,8 +1,9 @@
 
 
 ## Description: Creates new data frame with only majority peaks  
-## Input
-## Output
+## Input: A data frame of the counts split up by read and direction.
+## Output: Only the top two allele counts for each Locus, disregards stutter for heterozygous genotypes.
+
 cov_maj_peaks <- function (allele_counts_df) {
     allele_counts_df[is.na(allele_counts_df)] <- 0
     allele_counts_df$Coverage_of_Majority_Peaks <- (allele_counts_df$D2_R1 + allele_counts_df$D2_R2 + allele_counts_df$D1_R1 + allele_counts_df$D1_R2)
@@ -10,15 +11,19 @@ cov_maj_peaks <- function (allele_counts_df) {
                     group_by(Locus)  %>%  
                     top_n(2)
 }
+## Description: Assigns a genotype call to each locus based on the peak height ratio (20%).
+## Input: Data frame with only top two allele counts per locus.
+## Output: Data frame with extra column containing genotype call per locus.
 
-#preliminary code for genotype calls (.2 can be changed to any other ratio if needed)
 genotype_call <- function (maj_peak_df) {
     maj_peak_df %>% 
       group_by(Locus) %>% 
       mutate(Genotype = ifelse(min(Coverage_of_Majority_Peaks)/max(Coverage_of_Majority_Peaks) > 0.2, "Heterogyzous", "Homozygous"))
 }
 
-#preliminary code for PHR
+## Description: Assigns a peak height ratio to each heterozygous locus based on the top two peaks.
+## Input: Data frame with only top two allele counts per locus, and genotype call.
+## Output: Data frame with extra column containing the peak heigh ratio for heterzygous loci, and NA for homozygous loci. One PHR per locus.
 
 PHR <- function (genotype_call_df) {
       genotype_call_df %>% 
@@ -27,8 +32,10 @@ PHR <- function (genotype_call_df) {
                            round((min(Coverage_of_Majority_Peaks)/max(Coverage_of_Majority_Peaks)),4), NA_real_))
 }
 
+## Description: Assigns a read bias to each allele (two for heterzygotes and only the majority peak for homozygotes).
+## Input: Data frame with top two allele counts per locus, genotype call, and PHR per loci.
+## Output: Data frame with extra column containing the peak heigh ratio for heterzygous loci, and NA for homozygous loci. One PHR per locus.
 
-#preliminary code for Read Bias
 Read_bias <- function(PHR_df) {
     ungroup(PHR_df) %>% 
       mutate(Sum_R1 = (D2_R1 + D1_R1)) %>% 
