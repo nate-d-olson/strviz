51cbc0a729727375199009c8313243b1159c177f
Winston <sew@PN107082.campus.nist.gov>
2015-07-21 14:03:02 -0400

    added if statement so output dont keep appending

--- process_straitrazor_output.R
+++ process_straitrazor_output.R
@@ -93,7 +93,10 @@ process_sample <- function (seq_dir) {
 }
 
 batch_process_samples  <- function (sample_dirs) {
-    for (dirs in sample_dirs) {    
+    if (file.exists("summary.csv")){
+        unlink("summary.csv")
+    }
+    for (dirs in sample_dirs) {
         summary_met <- process_sample(dirs)
         ## this writes to the same file overwriting existing data 
         ## use append = TRUE to add to the file - need to be careful with this
