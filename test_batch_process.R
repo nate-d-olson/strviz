6bfcbdc7169416e95f03cf856a39e53812a14c93
Winston <sew@PN107082.campus.nist.gov>
2015-07-21 14:09:25 -0400

    added possible way to output summaries

--- test_batch_process.R
+++ test_batch_process.R
@@ -5,29 +5,23 @@ sample_dirs <- list.dirs("STRaitRazor2.0output", recursive = F) %>%
                     paste0("/PE_MiSeq_ALL")
 
 test_batch <- function (sample_dirs) {
+    if (file.exists("summary.csv")){
+        unlink("summary.csv")
+    }
     for (dirs in sample_dirs) {    
         summary_met <- process_sample(dirs)
-        ## this writes to the same file overwriting existing data 
-        ## use append = TRUE to add to the file - need to be careful with this
-        ## as it will add to the file even if it was from a previous run
-        ## think about how to avoid this issue
         write.table(summary_met, "summary.csv", append = TRUE, sep = ",", row.names = FALSE, col.names=!file.exists("summary.csv")) 
     }
 }
 
 
-## batch process fails
 test_batch(sample_dirs)
 
-
+## Possible way to output specific summaries 
 summary_read_bias <- function (sample_dirs) {
     for (dirs in sample_dirs) {    
         summary_met <- process_sample(dirs) 
         summary_met <- select(summary_met, Sample, Locus, Read_Bias)
-        ## this writes to the same file overwriting existing data 
-        ## use append = TRUE to add to the file - need to be careful with this
-        ## as it will add to the file even if it was from a previous run
-        ## think about how to avoid this issue
         write.table(summary_met, "summary_read_bias.csv", append = TRUE, sep = ",", col.names=!file.exists("summary_read_bias.csv"), row.names = FALSE) 
     }
 }
